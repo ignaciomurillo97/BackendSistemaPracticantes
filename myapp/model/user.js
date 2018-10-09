0e112d8ad8d7ef9c92d282efe7a38362f9b024dc
Alvaro <alvarocastro74@gmail.com>
2018-10-09 14:23:03 -0600

    user class

--- myapp/model/user.js
+++ myapp/model/user.js
@@ -1,3 +1,30 @@
 class User {
+    get userName() {
+        return this._userName;
+    }
+
+    set userName(value) {
+        this._userName = value;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+    get identification() {
+        return this._identification;
+    }
+
+    set identification(value) {
+        this._identification = value;
+    }
+    constructor(){
+        this._identification = 0;
+        this._userName = '';
+        this._password = '';
+    }
     
 }
--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,28 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var user = require('../db/userDB.js')
+let express = require('express');
+let router = express.Router();
+let UserBusiness = require('../db/userDB.js');
+let User = require('../model/user.js');
 
 /* GET home page. */
 router.post ('/', function(req, res, next){
     res.setHeader('Content-Type', 'application/json');
 
     let loginObject;
-    let username = req.body.username;
-    let password = req.body.password;
 
-    usuario.autenticar(username, password).then(function(dbResponse){
-        if (dbResponse.length > 0 && passwordHash.verify(password, dbResponse[0].Contraseña)) {
-            req.session.user = dbResponse[0];
-            redireccion = redireccionUsuario(dbResponse[0].Tipo)
-            loginObject = {'authenticate':true, 'redirect':redireccion};
-        }
-        else {
-            loginObject = {'authenticate':false, 'redirect': null};
-        }
-        res.send(loginObject);
-    })
+    let user = new User();
+    user.userName(req.body.userName);
+    user.password(req.body.password);
+
+    console.log(user);
+    // usuario.autenticar(username, password).then(function(dbResponse){
+    //     if (dbResponse.length > 0 && passwordHash.verify(password, dbResponse[0].Contraseña)) {
+    //         req.session.user = dbResponse[0];
+    //         redireccion = redireccionUsuario(dbResponse[0].Tipo)
+    //         loginObject = {'authenticate':true, 'redirect':redireccion};
+    //     }
+    //     else {
+    //         loginObject = {'authenticate':false, 'redirect': null};
+    //     }
+    //     res.send(loginObject);
+    // })
 });
 
-router.delete()
 
 module.exports = router;
